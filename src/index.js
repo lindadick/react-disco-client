d0d074c143c46dae2d7ae0066b71d022ca8fa8d9
Linda Dick <linda.j.dick@gmail.com>
2018-06-22 18:13:09 -0500

    WIP setting up Semantic-UI-less for a custom theme.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 
 import App from './components/App';
+import "semantic-ui-less/semantic.less";
 import './stylesheets/disco.scss';
 
 window.React = React
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const webpack = require("webpack");
 const path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: path.resolve(__dirname + "/src/index.js"),
@@ -7,6 +8,11 @@ module.exports = {
 		path: path.resolve(__dirname + "/public/assets"),
 		filename: "bundle.js",
 		publicPath: "assets"
+	},
+	resolve: {
+		alias: {
+		   '../../theme.config$': path.join(__dirname, 'disco-theme/theme.config')  
+		}
 	},
 	module: {
 		rules: [
@@ -32,9 +38,36 @@ module.exports = {
 			{
 				test: /\.scss$/,
 				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
-			}
+			},
+			{
+				use: ExtractTextPlugin.extract({
+				use: ['css-loader', 'less-loader']
+				}),
+				test: /\.less$/
+			},
+			{
+				test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+				use: 'file-loader?name=[name].[ext]?[hash]'
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+			},
+			{
+				test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: 'file-loader'
+			},
+			{
+			  test: /\.otf(\?.*)?$/,
+			  use: 'file-loader?name=/fonts/[name].  [ext]&mimetype=application/font-otf'
+			  }		
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin({
+		  filename: '[name].[contenthash].css',
+		})
+	 ]
 }
 
 
