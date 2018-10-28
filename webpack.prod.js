1d61af75be5996fda1237c527868f16a792d757e
Linda Dick <linda.j.dick@gmail.com>
2018-10-28 15:38:41 -0500

    Keep BundleAnalyzerPlugin import intact but commented out for later use.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const common = require('./webpack.common.js');
 
